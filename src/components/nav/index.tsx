4a14b9d7cf49578377147e156528b203cc9e63cf
olee2 <oleaeik@protonmail.com>
2023-02-25 20:05:31 +0100

    Hide menu when url changes

--- src/components/nav/index.tsx
+++ src/components/nav/index.tsx
@@ -1,11 +1,17 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { Link, useLocation } from "react-router-dom";
+
 import hamburger from "../../assets/hamburger.svg";
 import "./styles.scss";
 import Cart from "../cart";
 
 function Nav() {
+  const location = useLocation();
   const [display, setDisplay] = useState(false);
+
+  useEffect(() => {
+    setDisplay(false);
+  }, [location]);
 
   const handleDisplay = () => {
     setDisplay(!display);
