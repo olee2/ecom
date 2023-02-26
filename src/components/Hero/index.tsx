5b559b018acbfff7b857a84a7dadd45b5c2a05d3
olee2 <oleaeik@protonmail.com>
2023-02-26 19:54:04 +0100

    Adding hero

---
+++ src/components/Hero/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./styles.scss";
+
+function Hero() {
+  return (
+    <div className="hero">
+      <div className="inner-hero">
+        <div>
+          <h1>Welcome to Ecom</h1>
+          <p> - Buy yourself something pretty</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import "./styles.scss";
+import Hero from "../../components/Hero";
 
 function Home() {
   return (
     <main>
-      <h1>Home</h1>
+      <Hero />
     </main>
   );
 }
