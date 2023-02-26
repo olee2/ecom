a19b3144fc5dd2f74d17f93cb87c9d30a661b28f
olee2 <oleaeik@protonmail.com>
2023-02-26 20:11:05 +0100

    Adding footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,10 +1,20 @@
 import React from "react";
 import "./styles.scss";
+import Copyright from "../../assets/copyright.svg";
 
 function Footer() {
   return (
     <footer>
-      <p>Footer</p>
+      <div className="inner-container">
+        <div>
+          <h2>Ecom Ltd</h2>
+          <div className="copyright-container">
+            <p>Copyright</p>
+            <img src={Copyright} alt="" className="copyright" />
+            <p>2023. All rights reserved</p>
+          </div>
+        </div>
+      </div>
     </footer>
   );
 }
