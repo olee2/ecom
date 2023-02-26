91b5b44e1d1ddc9532f0791f710bb9025003ac7e
olee2 <oleaeik@protonmail.com>
2023-02-26 20:54:17 +0100

    Try again

--- src/components/nav/index.tsx
+++ src/components/nav/index.tsx
@@ -1,9 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { Link, useLocation } from "react-router-dom";
-
 import hamburger from "../../assets/hamburger.svg";
 import "./styles.scss";
-import Cart from "../Cart";
+import Cart from "../cart";
 
 function Nav() {
   const location = useLocation();
