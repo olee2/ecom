b8e0da9f975ac4ec2ee104651b367c8de5676fbe
olee2 <65301179+olee2@users.noreply.github.com>
2023-04-01 19:04:36 +0200

    Adding message if empty

--- src/pages/checkout/index.tsx
+++ src/pages/checkout/index.tsx
@@ -2,29 +2,40 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import RootState from "../../models/RootState";
-import CartItems from "../../components/cartItem";
+import CartItems from "../../components/cartItems";
 import "./checkout.scss";
 
 function Checkout() {
   const { total, amount } = useSelector((state: RootState) => state.cart);
   return (
     <main className="checkout-main">
-      <div className="inner-container">
-        <CartItems />
-        <div className="">
-          <h1>Shopping Bag</h1>
-          <div className="amount">
-            <p>{amount} product(s)</p>
+      {amount ? (
+        <div className="inner-container">
+          <CartItems />
+          <div className="checkout">
+            <h1>Shopping Bag</h1>
+            <div className="amount">
+              <p>
+                {amount} {amount > 1 ? "product(s)" : "product"}
+              </p>
+            </div>
+            <div className="total">
+              <p>Total:</p>
+              <p>{total} NOK</p>
+            </div>
+            <Link className="btn" to="success">
+              Checkout
+            </Link>
           </div>
-          <div className="total">
-            <p>Total:</p>
-            <p>{total} NOK</p>
-          </div>
-          <Link className="btn" to="success">
-            Checkout
+        </div>
+      ) : (
+        <div className="empty">
+          <h1>Empty Cart</h1>
+          <Link to="/" className="btn">
+            Go shopping
           </Link>
         </div>
-      </div>
+      )}
     </main>
   );
 }
